e9262a59425da727d0bf119f9438210614e61171
Dilara <unaldilaara@gmail.com>
2025-06-23 14:34:42 +0300

    Add to favorites button is added

--- Home-Page/script.js
+++ Home-Page/script.js
@@ -18,12 +18,17 @@ function cartCreator(book) {
       </div>
       <div class="thumb-content">
         <h4 class="book-name">${book.name}</h4>
-            ${book.stars}
+        ${book.stars}
         <p class="item-price"><strike>${book.lastPrice || ""}</strike><b>$${book.price}</b></p>
-        <button class="btn btn-outline-danger"> Add to Cart <i class="bi bi-bag"></i></button>
+        <div class="d-flex justify-content-between gap-2">
+          <button class="btn btn-outline-danger">Add to Cart <i class="bi bi-bag"></i></button>
+          <button class="btn btn-outline-primary favorite-btn" data-id="${book.id}">♡ Add to Favorites</button>
+        </div>
       </div>
-    </div>`
+    </div>
+    </div>`;
 }
+
 
 newestBookFiltred1.forEach(book => {
     newBooksCarouselItem1.innerHTML += (cartCreator(book));
